f3fc824588d1e4a947980334747eb8f208b26953
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-12 16:36:18 +0200

    make build

---
+++ index.428cdd86.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider"),l=(e.clientWidth-t.clientWidth)/2+"px",i=(e.clientHeight-t.clientHeight)/2+"px";t.style.top=l,t.style.left=i;
+//# sourceMappingURL=index.428cdd86.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = field.querySelector(".spider");
+const spiderPosTop = (field.clientWidth - spider.clientWidth) / 2 + "px";
+const spiderPosLeft = (field.clientHeight - spider.clientHeight) / 2 + "px";
+spider.style.top = spiderPosTop;
+spider.style.left = spiderPosLeft;
+
+//# sourceMappingURL=index.f75de5e1.js.map
